71adc1cd281c36c4ec69b91f5784d5230653af26
nim770241 <nim770241@gamil.com>
2026-06-26 01:01:18 +0300

    Made the code more readable and added Trim() for the PassFeel variable

--- Program.cs
+++ Program.cs
@@ -6,55 +6,67 @@
 		static void Main() {
 			
 			string? login;
-			string? fA;
+			string? PassFeel;
 			string? pass = "";
 			Random rf = new Random();
-			Random rs = new Random();
-			short tf = Convert.ToInt16(rf.Next(200, 2000));
-			short ts = Convert.ToInt16(rs.Next(300, 5000));
+			int Check = Convert.ToInt16(rf.Next(200, 2000));
+			int Boot = Convert.ToInt16(rf.Next(300, 5000));
 
 	Console.WriteLine("***Checking...");
-	System.Threading.Thread.Sleep(tf);
+	System.Threading.Thread.Sleep(Check);
+
 		Console.WriteLine("***Booting...");
-	System.Threading.Thread.Sleep(tf);
+	System.Threading.Thread.Sleep(Boot);
 		Console.WriteLine("***Done");
+
+
 		while(true) {
 	Console.WriteLine("****Please create the user");
 		login = Console.ReadLine();
+
 		if (!string.IsNullOrWhiteSpace(login)) 
 			break;
+
 	Console.WriteLine("Error: This field must not be empty");
 	}
+		
+		
 		while(true){
 	Console.WriteLine("****Add a password? Y/N");
-		fA = Console.ReadLine()?.ToLower();
+		PassFell = Console.ReadLine()?.Trim().ToLower();
 		if (!string.IsNullOrWhiteSpace(fA))
 			break;
 	Console.WriteLine("Error: This field must not be empty");
 		}
-			if (fA == "y") {
+
+			if (PassFell == "y") {
+
+
 				while(true) {
 			Console.WriteLine("****Write the pass");
 				pass = Console.ReadLine();
 				if (!string.IsNullOrWhiteSpace(pass))
 					break;
+
 			Console.WriteLine("Error: This field must not be empty");
 				}
 				Console.WriteLine("****Creating done");
 			}
+
 			else 
 				Console.WriteLine("****Creating done");
 			
-	Console.WriteLine("****Please login: ");
+	Console.WriteLine("\n****Please login: ");
 	string? logon = Console.ReadLine();
 		
+
 		while (logon != login) {
 			System.Threading.Thread.Sleep(400);
 				Console.WriteLine("Error: Please try again");
 				logon = Console.ReadLine();
 				}
 
-	switch(fA) {			
+	switch(PassFell) {			
 	case "y":
 	Console.WriteLine("***Write the password: ");
 	string? poss = Console.ReadLine();
@@ -64,12 +76,17 @@
 		poss = Console.ReadLine();
 	}
 		break;
+
 	default:
 		Console.WriteLine("****You haven't a passwd");
 		break;
-
 	}
+	
 	Console.WriteLine("*****Welcome to TestOS");
+	
+
+
+
 		}
 
 	}
