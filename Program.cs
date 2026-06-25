17b38780962d4242a3074e5d2ec780b9080e3b35
nim770241 <nim770241@gamil.com>
2026-06-26 00:04:17 +0300

    made better login system, visual

--- Program.cs
+++ Program.cs
@@ -8,50 +8,68 @@
 			string? login;
 			string? fA;
 			string? pass = "";
-
+			Random rf = new Random();
+			Random rs = new Random();
+			short tf = Convert.ToInt16(rf.Next(200, 2000));
+			short ts = Convert.ToInt16(rs.Next(300, 5000));
 
 	Console.WriteLine("***Checking...");
-	System.Threading.Thread.Sleep(200);
+	System.Threading.Thread.Sleep(tf);
 		Console.WriteLine("***Booting...");
-	System.Threading.Thread.Sleep(2000);
+	System.Threading.Thread.Sleep(tf);
 		Console.WriteLine("***Done");
-	Console.WriteLine("Please create the user");
+		while(true) {
+	Console.WriteLine("****Please create the user");
 		login = Console.ReadLine();
-	Console.WriteLine("Add a password? Y/N");
+		if (!string.IsNullOrWhiteSpace(login)) 
+			break;
+	Console.WriteLine("Error: This field must not be empty");
+	}
+		while(true){
+	Console.WriteLine("****Add a password? Y/N");
 		fA = Console.ReadLine()?.ToLower();
+		if (!string.IsNullOrWhiteSpace(fA))
+			break;
+	Console.WriteLine("Error: This field must not be empty");
+		}
 			if (fA == "y") {
-			Console.WriteLine("Write the pass");
+				while(true) {
+			Console.WriteLine("****Write the pass");
 				pass = Console.ReadLine();
-				Console.WriteLine("Creating done");
+				if (!string.IsNullOrWhiteSpace(pass))
+					break;
+			Console.WriteLine("Error: This field must not be empty");
+				}
+				Console.WriteLine("****Creating done");
 			}
 			else 
-				Console.WriteLine("Creating done");
+				Console.WriteLine("****Creating done");
 			
-	Console.WriteLine("Please login: ");
+	Console.WriteLine("****Please login: ");
 	string? logon = Console.ReadLine();
 		
 		while (logon != login) {
 			System.Threading.Thread.Sleep(400);
-				Console.WriteLine("Please try again");
+				Console.WriteLine("Error: Please try again");
 				logon = Console.ReadLine();
 				}
 
 	switch(fA) {			
 	case "y":
-	Console.WriteLine("Write the password: ");
+	Console.WriteLine("***Write the password: ");
 	string? poss = Console.ReadLine();
 		
 	while(poss != pass) {
-		Console.WriteLine("Try again");
+		Console.WriteLine("Error: Try again");
 		poss = Console.ReadLine();
 	}
 		break;
 	default:
-		Console.WriteLine("You haven't a passwd");
+		Console.WriteLine("****You haven't a passwd");
 		break;
 
 	}
-	Console.WriteLine("Welcome to TestOS");
+	Console.WriteLine("*****Welcome to TestOS");
 		}
 
 	}
